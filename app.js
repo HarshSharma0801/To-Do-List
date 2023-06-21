55639f6aedf0cc54785757bcdca811bf069cc0ac
HarshSharma0801 <121893196+HarshSharma0801@users.noreply.github.com>
2023-06-22 02:53:55 +0530

    update

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const app = express();
-const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
+const ejs  = require('ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname + '/public'));
+app.set('view engine','ejs');
 
-
+var items =[] ;
 app.get('/',(req,res)=>{
-  res.sendFile(__dirname+'index.html');
-  
+ var today = new Date();
+ var options = {
+   weekday:"long",
+   day:"numeric",
+   month:"long"
+ }
+ var day = today.toLocaleDateString("en-US",options);
+
+ res.render("index",{MainDay:day , Task:items});
+
+
+})
+
+app.post("/",(req,res)=>{
+  var item = req.body.NewTask;
+   items.push(item);
+   console.log(req.body.NewTask);
+   res.redirect('/');
+
 })
 
 app.listen(3000,()=>{
